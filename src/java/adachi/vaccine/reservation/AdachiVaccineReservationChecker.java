22d857ab5a0dd19f3288161930f8bd235f1affef
Tanishy <mambaout134@gmail.com>
2021-09-01 22:31:34 +0900

    Initial commit

---
+++ src/java/adachi/vaccine/reservation/AdachiVaccineReservationChecker.java
@@ -0,0 +1,95 @@
+package adachi.vaccine.reservation;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import java.util.stream.IntStream;
+
+public class AdachiVaccineReservationChecker {
+
+    private static final Logger logger = Logger.getLogger("AdachiVaccineReservationChecker");
+
+    private static final HttpClient client = HttpClient.newBuilder().build();
+
+    private static final Path BASE_DIR = Path.of("out");
+
+    private static final Path HTML_DIR = Path.of(BASE_DIR.toString(), "html");
+
+    private static final Path LOG_DIR = Path.of(BASE_DIR.toString(), "log");
+
+    public static void main(String[] args) {
+        // ロガーの設定
+        logger.setLevel(Level.INFO);
+        try {
+            FileHandler handler = new FileHandler(LOG_DIR + "\\adachi-vaccine-reservation-checker.log");
+            handler.setFormatter(new SimpleFormatter());
+            logger.addHandler(handler);
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "ロガーの設定でエラー", e);
+        }
+
+        logger.log(Level.INFO, "足立区コロナワクチン予約空き状況確認 開始");
+        LocalDate today = LocalDate.now();
+        int year = today.getYear();
+        int month = today.getMonthValue();
+        int[] months = { month, month + 1 };
+        IntStream.of(months).forEach(m -> check(year, m));
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        logger.log(Level.INFO, "足立区コロナワクチン予約空き状況確認 終了");
+    }
+
+    private static void check(int year, int month) {
+        HttpRequest req = HttpRequest.newBuilder(URI.create("https://adachi.hbf-rsv.jp/mypage/status/?year=%s&month=%s".formatted(year, month))).build();
+        client.sendAsync(req, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(body -> {
+                    Path outputDir = Path.of(HTML_DIR + "\\%s月".formatted(month));
+                    if (Files.notExists(outputDir, LinkOption.NOFOLLOW_LINKS)) {
+                        try {
+                            Files.createDirectory(outputDir);
+                        } catch (IOException e) {
+                            logger.log(Level.SEVERE, "HTMLファイル出力ディレクトリ作成失敗", e);
+                            return;
+                        }
+                    } else {
+                        try {
+                            Files.list(outputDir).forEach(file -> {
+                                try {
+                                    Files.delete(file);
+                                } catch (IOException e) {
+                                    logger.log(Level.WARNING, "ファイルの削除に失敗 : %s".formatted(file.getFileName()), e);
+                                }
+                            });
+                        } catch (IOException e) {
+                            logger.log(Level.WARNING, "古いHTMLファイルの削除に失敗", e);
+                        }
+                    }
+                    Path filePath = Path.of(outputDir + "\\足立区コロナワクチン予約空き状況（%s月）- %s.html".formatted(month, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddhhmmss"))));
+                    try (FileWriter fw = new FileWriter(filePath.toString())) {
+                        logger.log(Level.INFO, "HTMLファイル出力 開始");
+                        fw.write(body);
+                        logger.log(Level.INFO, "HTMLファイル出力 終了");
+                        logger.log(Level.INFO, "ファイル出力先：%s".formatted(filePath.toString()));
+                    } catch (IOException e) {
+                        logger.log(Level.SEVERE, "HTMLファイル出力失敗", e);
+                    }
+                });
+    }
+}
